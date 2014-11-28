f1c14ac8af23df91efcc81914cc3380746ebd35b
nyaigoon <nyaigoon@outlook.com>
2014-11-28 14:08:40 +0900

    JFrame사용 계산기창 구성 및 클로즈 오퍼레이터로 끌수있도록 숫자 더하기 빼기 곱하기 나누기 등호 클리어 등 구성

--- Calculator/CalcClass.java
+++ Calculator/CalcClass.java
@@ -1,9 +1,9 @@
 package Calculator;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
-
 
 public class CalcClass extends JFrame {
 	JLabel label;         //계산결과 출력부분
@@ -14,10 +14,40 @@ public class CalcClass extends JFrame {
 	int result;    //중간 계산 결과
 	char last;   //이전 선택한 연산자
 	
-	public CalcClass() {
-		
-	}
+ public CalcClass() {
+	 super("계산기");
+	  setBounds(200, 200, 300,300);
+	  setResizable(false);
+	  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	  label = new JLabel("0", JLabel.RIGHT);
+	  
+
+	  add(label, BorderLayout.NORTH);
+
+	  JPanel p = new JPanel(new GridLayout(4,4,1,1));
+	  add(p, BorderLayout.CENTER);
+
+	  plus = new JButton("+");
+	  minus = new JButton("-");
+	  multi = new JButton("*");
+	  div  = new JButton("/");
+	  equal = new JButton("=");
+	  clear = new JButton("C");
+
+	 
+	  for (int i = 0; i < bNum.length; i++) {
+	   bNum[i] = new JButton(Integer.toString(i));
+	  }
+
+	  p.add(bNum[7]); p.add(bNum[8]); p.add(bNum[9]); p.add(plus);
+	  p.add(bNum[4]); p.add(bNum[5]); p.add(bNum[6]); p.add(minus);
+	  p.add(bNum[1]); p.add(bNum[2]); p.add(bNum[3]); p.add(multi);
+	  p.add(bNum[0]); p.add(equal); p.add(clear); p.add(div);
+	 }
  public static void main(String[] args) {
 
+	new CalcClass();
  }
+
 }
