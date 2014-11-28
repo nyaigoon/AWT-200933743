f90e60979ae962ff5ef3cbfb8e723aba81dce2a6
nyaigoon <nyaigoon@outlook.com>
2014-11-28 14:01:35 +0900

    import 및 CalcClass JFrame 상속 필요 변수들 선언
    Public 으로 CalcClass클래스 생성

--- Calculator/CalcClass.java
+++ Calculator/CalcClass.java
@@ -1,6 +1,22 @@
 package Calculator;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 
-public class CalcClass{
+
+public class CalcClass extends JFrame {
+	JLabel label;         //계산결과 출력부분
+	JButton bNum[] = new JButton[10];    //숫자 버튼
+	JButton plus,minus, multi, div, equal, clear; //연산 버튼
+	
+	String inputValue;  //현재 입력중인 숫자
+	int result;    //중간 계산 결과
+	char last;   //이전 선택한 연산자
+	
+	public CalcClass() {
+		
+	}
  public static void main(String[] args) {
 
  }
