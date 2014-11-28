9fab1cc78541a67270e32d1d39a0f36e1ebb47ef
nyaigoon <nyaigoon@outlook.com>
2014-11-28 14:18:44 +0900

    이벤트 리스너 연결 이벤트 처리

--- Calculator/CalcClass.java
+++ Calculator/CalcClass.java
@@ -14,6 +14,10 @@ public class CalcClass extends JFrame {
 	int result;    //중간 계산 결과
 	char last;   //이전 선택한 연산자
 	
+public static void main(String[] args) {
+
+	new CalcClass();
+ }
  public CalcClass() {
 	 super("계산기");
 	  setBounds(200, 200, 300,300);
@@ -44,10 +48,59 @@ public class CalcClass extends JFrame {
 	  p.add(bNum[4]); p.add(bNum[5]); p.add(bNum[6]); p.add(minus);
 	  p.add(bNum[1]); p.add(bNum[2]); p.add(bNum[3]); p.add(multi);
 	  p.add(bNum[0]); p.add(equal); p.add(clear); p.add(div);
+	  
+	  //이벤트 리스너 연결
+	  Number iven = new Number();
+	  for (int i = 0; i < bNum.length; i++) {
+	   bNum[i].addActionListener(iven);
+	  }
+
+	  Calc a = new Calc();
+	  plus.addActionListener(a);
+	  minus.addActionListener(a);
+	  multi.addActionListener(a);
+	  div.addActionListener(a);
+	  equal.addActionListener(a);
+	  clear.addActionListener(a);
+
+	  setVisible(true);
 	 }
- public static void main(String[] args) {
 
-	new CalcClass();
- }
+	 //숫자버튼의 이벤트 처리
+	 class Number implements ActionListener {
+	  public void actionPerformed(ActionEvent e) {
+	   String s = e.getActionCommand(); //클릭한 버튼의 레이블
+	   if (inputValue == null) {
+	    if (s.equals("0")) return;  //첫 자리이면서 0이면 무시
+	    else inputValue = new String(); //첫 자리이면 String 객체 생성
+	   }
+	   if (inputValue.length() >= 9) return; //9자리 넘으면 무시
+	   inputValue += s;     //두번째 이후이면 누른 숫자 덧붙이기
+	   label.setText(inputValue);   //현재 숫자 화면에 표시
+	  }
+	 }
 
-}
+	 //기타 계산 버튼의 이벤트 처리
+	 class Calc implements ActionListener {
+	  public void actionPerformed(ActionEvent e) {
+	   JButton source = (JButton) e.getSource();
+	   int value;
+
+
+	   //기타 계산 +,-,*,/
+	   if (inputValue != null) {
+	    value = Integer.parseInt(inputValue);
+	    switch (last) {
+	     case '+' : result += value; break;
+	     case '-' : result -= value; break;
+	     case '*' : result *= value; break;
+	     case '/' : result /= value; break;
+	     default  : result = value; break;
+	    }
+	    label.setText(Integer.toString(result));
+	   }
+	   inputValue = null;
+	   last = source.getText().charAt(0);
+	  }
+	 }
+	}
